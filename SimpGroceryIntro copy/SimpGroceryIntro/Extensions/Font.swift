08142f310fe6a83f4ef73588c4bd126768f63d1f
Hedy <hedyli811@gmail.com>
2025-03-19 15:22:42 -0300

    MVVM

---
+++ SimpGroceryIntro copy/SimpGroceryIntro/Extensions/Font.swift
@@ -0,0 +1,16 @@
+//
+//  Font.swift
+//  SimpGroceryIntro
+//
+//  Created by LI, Hedy on 2025-03-19.
+//
+
+import SwiftUI
+ extension Font {
+ static let titleFont = Font.system(size: 28, weight: .black,design: .default)
+ static let sectonHeaderFont = Font.system(size: 28,
+weight: .black,design: .default)
+ static let bodyFont = Font.system(size: 28, weight: .black,design: .default)
+ static let buttonFont = Font.system(size: 22, weight: .black,design: .default)
+ static let inputFont = Font.system(size: 22, weight: .black,design: .default)
+ }
